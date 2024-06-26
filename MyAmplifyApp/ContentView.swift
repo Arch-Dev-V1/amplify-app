8e62bffcd912f18918d171e3c946a32f7440b5f7
Arch-Dev-V1 <ashutoshbhatt599@gmail.com>
2024-06-26 18:22:45 +0530

    first commit

---
+++ MyAmplifyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyAmplifyApp
+//
+//  Created by Ashutosh Bhatt on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyAmplifyApp/MyAmplifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmplifyAppApp.swift
+//  MyAmplifyApp
+//
+//  Created by Ashutosh Bhatt on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAmplifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
